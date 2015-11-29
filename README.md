e891071377d14a2ac7ca5f3c86a69c84f5c7915f
terrytangyuan <terrytangyuan@gmail.com>
2015-11-29 13:20:58 -0600

    setup travis for different os

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+[![Travis-CI Build Status](https://travis-ci.org/terrytangyuan/rflow.svg?branch=master)](https://travis-ci.org/terrytangyuan/rflow)
+[![License](http://img.shields.io/:license-mit-blue.svg?style=flat)](http://badges.mit-license.org)
+
 rflow - Pipeline Interface for TensorFlow in R
 
 This is an experimental and work-in-progress R library for easily using Google TensorFlow, inspired by Google skflow package.  
--- travis_install.sh
+++ travis_install.sh
@@ -5,16 +5,22 @@ deactivate
 virtualenv --system-site-packages testenv
 source testenv/bin/activate
 
-# dependency installation
+# Python dependencies
 sudo pip install --upgrade pip
 sudo pip install numpy
 sudo pip install SciPy
 sudo pip install pandas
 sudo pip install sklearn
-sudo pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.5.0-cp27-none-linux_x86_64.whl
+# tensorflow for separate os
+if [ ${TRAVIS_OS_NAME} == "linux" ]; then
+  sudo pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.5.0-cp27-none-linux_x86_64.whl
+fi
+if [ ${TRAVIS_OS_NAME} == "osx" ]; then
+  sudo pip install https://storage.googleapis.com/tensorflow/mac/tensorflow-0.5.0-py2-none-any.whl
+fi
 sudo pip install git+git://github.com/google/skflow.git
 
-
+# R dependencies
 curl -OL http://raw.github.com/craigcitro/r-travis/master/scripts/travis-tool.sh
 chmod 755 ./travis-tool.sh
 ./travis-tool.sh bootstrap
