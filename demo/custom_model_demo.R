90cd7a87a5141e50582162baeb4d8a416e4cee0b
terrytangyuan <terrytangyuan@gmail.com>
2015-11-29 19:18:39 -0600

    Added demo for custom_model

---
+++ demo/custom_model_demo.R
@@ -0,0 +1,17 @@
+# Customized DNN model in R
+
+predictors <- iris[1:4]
+target <- iris[,5]
+n_classes <- length(unique(target))
+
+rflowPipeline(eval_metric = 'accuracy_score',
+              test_percent = 0.25,
+              # write a custom model that will be passed to TensorFlowEstimator
+              # a DNN model with hidden layers of 12, 18, 12 and 0.45 probability of dropout
+              customModelWriter(
+                funcInput = c('X', 'y'),
+                returnValue = 'skflow.models.logistic_regression(X, y)',
+                TensorTransformation('skflow.ops.dnn', c(12, 18, 12), 0.45)),
+              TensorFlowEstimator(n_classes = n_classes),
+              preparePredictors(predictors),
+              prepareTargetVar(target))
--- tests/testthat/test_demo.R
+++ tests/testthat/test_demo.R
@@ -1,3 +1,10 @@
 context('Testing demos')
+
+context('1. Testing cnn demo')
 source("../../demo/cnn_demo.R")
+
+context('2. Testing dnn demo')
 source("../../demo/dnn_demo.R")
+
+context('3. Testing custom_model demo')
+source("../../demo/custom_model_demo.R")
