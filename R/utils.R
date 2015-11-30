2a42e3fe54ebca95cd7309f7b2ae77d3203f398b
terrytangyuan <terrytangyuan@gmail.com>
2015-11-29 20:34:03 -0600

    enable named arg in insertPyObjsStr (+2 squashed commits) Squashed commits: [9fec426] named args in insertPyObjsStr [2fae5b5] enable named arg in insertPyObjsStr

--- R/utils.R
+++ R/utils.R
@@ -8,6 +8,7 @@ createArgs <- function(names, getFunc=get){
   })), collapse = ", ")
 }
 
+
 # c(1,2,3) => [1,2,3]
 toPyObjStr <- function(rObj){
   python.assign('tmp_var', rObj)
@@ -20,6 +21,7 @@ toPyObjStr <- function(rObj){
   unlink('tmp_var.txt')
   return(pyObjStr)
 }
+
 
 # save a Python object to a text file
 savePyObjToFile <- function(pyVarName){
@@ -34,6 +36,7 @@ with open("%s", "w") as f:
 \tf.close()', fileName, pyVarName, pyVarName, pyVarName))
 }
 
+
 # load a python string from a file to an R object
 loadPyStrToR <- function(pyVarName){
   fileName <- paste0(pyVarName, '.txt')
@@ -41,19 +44,37 @@ loadPyStrToR <- function(pyVarName){
   return(python.get(pyVarName))
 }
 
+
 # for annonymous args
 # insertPyObjsStr(3, c(1,2,3))  => "3, [1, 2, 3]"
 insertPyObjsStr <- function(...){
   args <- list(...)
-  paste(unlist(lapply(args, function(arg){
+  namedArgsInds <- names(args) != ""
+  namedArgs <- names(args)[namedArgsInds]
+
+  namedStr <- paste(unlist(lapply(namedArgs, function(name){
+    paste0(name, '=', args[[name]])
+  })), collapse = ", ")
+
+  if(length(namedArgsInds) == 0){
+    unNamedArgs <- args
+  } else {
+    unNamedArgs <- args[!namedArgsInds]
+  }
+  unNamedStr <- paste(unlist(lapply(unNamedArgs, function(arg){
     toPyObjStr(arg)
   })), collapse = ", ")
+
+  paste0(unNamedStr, ifelse(namedStr == "", '', ', '), namedStr)
 }
 
-# createFuncStr('f', 3, c(1,2,3))  => "f(3, [1, 2, 3])"
+
+# createFuncStr('f', 3, c(1,2,3), prob=3)  => "f(3, [1, 2, 3], prob=3)"
 createFuncStr <- function(funcName, ...){
-  paste0(funcName, sprintf('(%s)', insertPyObjsStr(...)))
+  argStr <- insertPyObjsStr(...)
+  paste0(funcName, sprintf('(%s)', argStr))
 }
+
 
 # for named arguments
 additionalArgs <- function(theDots){
@@ -67,6 +88,7 @@ funcWriter <- function(body, funcHeader= 'def f():', returnValue = NULL){
   cat(paste0("\treturn(", returnValue, ")\n"))
 }
 
+
 # not used yet
 # funcExecuteWriter('X', 'f', 'X', 3, c(1,2,3))  => "X = f(X, 3, [1, 2, 3])\n"
 funcExecuteWriter <- function(LHS, funcName, firstArg, ...){
@@ -74,15 +96,17 @@ funcExecuteWriter <- function(LHS, funcName, firstArg, ...){
          LHS, funcName, firstArg, insertPyObjsStr(...))
 }
 
+
 # tabFuncExecuteWriter('X', 'f', 'X', 3, c(1,2,3))
 tabFuncExecuteWriter <- function(LHS, funcName, firstArg, ...){
   sprintf("\t%s = %s(%s, %s)\n",
           LHS, funcName, firstArg, insertPyObjsStr(...))
 }
 
+
 # customModelWriter(returnValue = "", funcInput = c('X', 'y'),
-#                   tabFuncExecuteWriter('X', 'f1', 'X', 3, c(1,2,3)),
-#                   tabFuncExecuteWriter('y', 'f2', 'X', 3, c(1,2,3)))
+                  # tabFuncExecuteWriter('X', 'f1', 'X', 3, c(1,2,3)),
+                  # tabFuncExecuteWriter('y', 'f2', 'X', 3, c(1,2,3)))
 # ==>
 # def custom_model(X,y):
 #   X = f1(X, 3, [1, 2, 3])
